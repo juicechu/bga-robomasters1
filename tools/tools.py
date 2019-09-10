6fc80c15907d513eb9c4cec9a2878f66a67ac8e8
Bruno Albuquerque <bga@gmail.com>
2019-09-10 16:11:46 +0000

    Added elapsed timer function.

--- tools/tools.py
+++ tools/tools.py
@@ -2,3 +2,9 @@
 def timer_ctrl(behavior_enum):
     print(f'tools.timer_ctrl({behavior_enum})')
 
+# Objective: Obtains the total time elapsed from when the timer first started to
+# the current time (in seconds)
+def timer_current():
+    print('tools.timer_current()')
+    return 0.0
+
