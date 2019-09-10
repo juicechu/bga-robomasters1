6ed3263218be761eb58e39e520446efc76822a00
Bruno Albuquerque <bga@gmail.com>
2019-09-10 16:07:44 +0000

    Added zoom control function.

---
+++ media_ctrl/media_ctrl.py
@@ -0,0 +1,6 @@
+# Objective: Applies higher magnifications to support visual recognition over
+# longer distances, enabling the robot to focus on unclear objects more
+# accurately
+def zoom_value_update(value):
+    print(f'media_ctrl.zoom_value_update({value})')
+
