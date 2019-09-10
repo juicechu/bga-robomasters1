f706bfc1f0d07d40f9a8b507643b8299563b288d
Bruno Albuquerque <bga@gmail.com>
2019-09-10 16:02:12 +0000

    Added timer control function.

--- rm_define/rm_define.py
+++ rm_define/rm_define.py
@@ -3,6 +3,11 @@ robot_mode_gimbal_follow = 1
 robot_mode_chassis_follow = 2
 robot_mode_free = 3
 
+# Timer behavior "enum".
+timer_start = 1
+timer_stop = 2
+timer_reset = 3
+
 # Objective: Sets the travel mode
 #
 # * Chassis Lead Mode: The gimbal follows the chassis to rotate along the yaw
@@ -11,6 +16,4 @@ robot_mode_free = 3
 #   axis.
 # * Free Mode: The gimbal and the chassis move without affecting each other.
 def robot_set_mode(mode_enum):
-    # Intentionally do nothing for now. We could potentially keep track of
-    # robot state in case we want to do some actual real similation of it.
     print(f'rm_define.robot_set_mode({mode_enum})')
---
+++ tools/tools.py
@@ -0,0 +1,4 @@
+# Objective: Starts, pauses, or stops the timer
+def timer_ctrl(behavior_enum):
+    print(f'tools.timer_ctrl({behavior_enum})')
+
