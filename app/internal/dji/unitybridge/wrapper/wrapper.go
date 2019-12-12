df840751ef0eb432f40d2c27ace89a7fe63cd2a2
Bruno Albuquerque <bga@gmail.com>
2019-12-12 17:12:29 +0000

    Added stub methods to fallback wrapper.
    - This prevents compilation errors.

--- app/internal/dji/unitybridge/wrapper/wrapper.go
+++ app/internal/dji/unitybridge/wrapper/wrapper.go
@@ -2,9 +2,37 @@
 
 package wrapper
 
-type wrapper struct{}
+type wrapper struct {}
+
+type UnityEventCallbackFunc func()
 
 func Instance() *wrapper {
 	panic("Only windows amd64 is supported.")
+}
+
+func (w *wrapper) CreateUnityBridge(name string, debuggable bool) {
+}
+
+func (w *wrapper) DestroyUnityBridge() {
+}
+
+func (w *wrapper) UnityBridgeInitialize() bool {
+	return false
+}
+
+func (w *wrapper) UnityBridgeUninitialize() {
+}
+
+func (w *wrapper) UnitySetEventCallback(e int64,
+	callback UnityEventCallbackFunc) {
+}
+
+func (w *wrapper) UnitySendEvent(e uint64, info []byte, tag uint64) {
+}
+
+func (w *wrapper) UnitySendEventWithString(e uint64, info string, tag uint64) {
+}
+
+func (w *wrapper) UnitySendEventWithNumber(e, info, tag uint64) {
 }
 
