393293b8be84e3deda893e7a34857bda03c3bc3d
Bruno Albuquerque <bga@bug-br.org.br>
2019-12-14 10:42:50 -0800

    Add some comment.

--- app/internal/dji/unitybridge/unitybridge_event_callback.c
+++ app/internal/dji/unitybridge/unitybridge_event_callback.c
@@ -5,6 +5,7 @@ extern void UnityEventCallbackGo(GoUint64 e, GoSlice info, GoUint64 tag);
 
 void UnityEventCallback(unsigned long long e, void* info, int length,
 		unsigned long long tag) {
+	// Create a Go slice with the info data.
 	GoSlice info_slice;
 	info_slice.data = info;
 	info_slice.len = length;
