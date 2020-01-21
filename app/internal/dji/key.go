779b6330406574f4b04e0122b50309579537b21e
Bruno Albuquerque <bga@bug-br.org.br>
2020-01-21 14:25:22 -0800

    Add keys that will be used for controlling the robot.
    - This will use the FPV joystick mode method.

--- app/internal/dji/key.go
+++ app/internal/dji/key.go
@@ -10,10 +10,13 @@ const (
 	KeyGimbalAngleFrontYawRotation
 	KeyGimbalAngleFrontPitchRotation
 	KeyGimbalConnection
+	KeyGimbalControlMode
 	KeyGimbalOpenAttitudeUpdates
 	KeyGimbalResetPosition
 	KeyMainControllerConnection
+	KeyMainControllerVirtualStick
 	KeyRobomasterOpenChassisSpeedUpdates
+	KeyRobomasterSystemFunctionEnable
 	KeysCount
 	// TODO(bga): Add keys here as needed.
 )
@@ -24,6 +27,8 @@ const (
 	KeyDataTypeBool DataType = iota
 	KeyDataTypeLong
 	KeyDataTypeAbsoluteRotationParameter
+	KeyDataTypeControllerParameter
+	KeyDataTypeFunctionEnableParameter
 	KeyDataTypeVoid
 	// TODO(bga): Add data types here as needed.
 )
@@ -31,8 +36,8 @@ const (
 type AccessType int
 
 const (
-	KeyAccessTypeNone AccessType = 1 << iota
-	KeyAccessTypeRead
+	KeyAccessTypeNone AccessType = 0
+	KeyAccessTypeRead AccessType = 1 << (iota - 1)
 	KeyAccessTypeWrite
 	KeyAccessTypeAction
 )
@@ -61,33 +66,43 @@ var (
 			KeyAccessTypeRead},
 		KeyGimbalConnection: keyAttributes{67108865, KeyDataTypeBool,
 			KeyAccessTypeRead},
+		KeyGimbalControlMode: keyAttributes{67108869, KeyDataTypeLong,
+			KeyAccessTypeRead | KeyAccessTypeWrite},
 		KeyGimbalAngleFrontYawRotation: keyAttributes{67108876,
 			KeyDataTypeAbsoluteRotationParameter,
 			KeyAccessTypeAction},
 		KeyGimbalAngleFrontPitchRotation: keyAttributes{67108877,
 			KeyDataTypeAbsoluteRotationParameter,
 			KeyAccessTypeAction},
-		KeyGimbalOpenAttitudeUpdates: keyAttributes{67108882, KeyDataTypeVoid,
-			KeyAccessTypeAction},
+		KeyGimbalOpenAttitudeUpdates: keyAttributes{67108882,
+			KeyDataTypeVoid, KeyAccessTypeAction},
 		KeyGimbalResetPosition: keyAttributes{67108870, KeyDataTypeLong,
 			KeyAccessTypeAction},
-		KeyMainControllerConnection: keyAttributes{33554433, KeyDataTypeBool,
-			KeyAccessTypeRead},
-		KeyRobomasterOpenChassisSpeedUpdates: keyAttributes{33554474, KeyDataTypeVoid,
+		KeyMainControllerConnection: keyAttributes{33554433,
+			KeyDataTypeBool, KeyAccessTypeRead},
+		KeyMainControllerVirtualStick: keyAttributes{33554436,
+			KeyDataTypeControllerParameter, KeyAccessTypeAction},
+		KeyRobomasterOpenChassisSpeedUpdates: keyAttributes{33554474,
+			KeyDataTypeVoid, KeyAccessTypeAction},
+		KeyRobomasterSystemFunctionEnable: keyAttributes{83886147,
+			KeyDataTypeFunctionEnableParameter,
 			KeyAccessTypeAction},
-		// TODO(bga): Add other attributes here as needed. Needs to be kept in
-		// 	sync with existing Keys.
+		// TODO(bga): Add other attributes here as needed. Needs to be
+		//  kept in sync with existing Keys.
 	}
 
 	keyByValueMap = map[int]Key{
 		117440513: KeyAirLinkConnection,
 		67108865:  KeyGimbalConnection,
+		67108869:  KeyGimbalControlMode,
 		67108876:  KeyGimbalAngleFrontYawRotation,
 		67108877:  KeyGimbalAngleFrontPitchRotation,
 		67108882:  KeyGimbalOpenAttitudeUpdates,
 		67108870:  KeyGimbalResetPosition,
 		33554433:  KeyMainControllerConnection,
+		33554436:  KeyMainControllerVirtualStick,
 		33554474:  KeyRobomasterOpenChassisSpeedUpdates,
+		83886147:  KeyRobomasterSystemFunctionEnable,
 	}
 )
 
